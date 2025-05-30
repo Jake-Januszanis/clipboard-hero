5b2458c715f7052ea9e5ad5e3342a2c22eccbc43
Jake Januszanis <jakejanuszanis@Jakes-MacBook-Air.local>
2025-05-30 15:19:33 -0700

    Setup basic add/remove functionality for session storage.

--- scripts/content.js
+++ scripts/content.js
@@ -9,30 +9,71 @@ window.addEventListener('DOMContentLoaded', function() {
             submitButton.addEventListener('click', (e) => {
                 e.preventDefault();
                 let textInputVal = popupContainerElement.getElementsByClassName('textInput').item(0).value
-                addNewListItem(textInputVal)
+                addItemToSessionStorage(textInputVal);
+            })
+        }
+        let loggerBtn = popupContainerElement.getElementsByClassName('log-btn').item(0);
+        if(loggerBtn) {
+            loggerBtn.addEventListener('click',() => {
+                chrome.storage.session.get().then((result) => {
+                    console.log(result)
+                })
             })
         }
     }
 });
 
+function addItemToSessionStorage(text) {
+    if(chrome) {
+        let randomId = Math.random().toString().split('.')[1];
+        let newObj = {}
+        newObj[randomId] = text;
+        chrome.storage.session.set(newObj).then(() => {
+            console.log('Value was set')
+        })
+    }
+}
 
-function addNewListItem(text) {
+function removeItemFromSessionStorage(key) {
+    chrome.storage.session.remove(key);
+}
+
+
+if(chrome) {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+        let keys = Object.keys(changes)
+        console.log(changes)
+        keys.forEach((k) => {
+            if(changes[k].newValue) {
+                return addNewListItem(changes[k].newValue, k);
+            }
+       })
+    })
+}
+
+function addNewListItem(text, id) {
     let listEle = document.getElementById('list')
     let itemContainer = document.createElement('div')
     itemContainer.setAttribute('class', 'itemContainer')
+
     let itemText = document.createElement('p')
+    itemText.textContent = text;
+    //Create a delete button
     let itemDeleteBtn = document.createElement('button')
+    // itemDeleteBtn.setAttribute('class', `${text.toLowerCase()}-btn`)
+    itemDeleteBtn.setAttribute('id', id)
+    itemDeleteBtn.textContent = 'X'
+    itemDeleteBtn.addEventListener('click', (e) => {
+        removeItemFromSessionStorage(e.target.id)
+        itemContainer.remove();
+    }, true)
+    //Create button that copys line of text
     let itemCopyBtn = document.createElement('button')
     itemCopyBtn.textContent = 'Copy'
     itemCopyBtn.addEventListener('click', () => {
         navigator.clipboard.writeText(text);
-    })
-    itemDeleteBtn.setAttribute('class', `${text.toLowerCase()}-btn`)
-    itemDeleteBtn.textContent = 'X'
-    itemDeleteBtn.addEventListener('click', (e) => {
-        itemContainer.remove();
     }, true)
-    itemText.textContent = text;
+    //Append Text/buttons to the container
     itemContainer.appendChild(itemText);
     itemContainer.appendChild(itemCopyBtn);
     itemContainer.appendChild(itemDeleteBtn);
